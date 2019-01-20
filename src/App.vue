66ffa81170773e11b082210d0f7fa773001abb1b
Aulon Mujaj <aulon.mujaj@gmail.com>
2019-01-19 19:14:03 -0500

    removed navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,7 @@
     <Snackbar />
     <v-toolbar
       app
-      :clipped-left="true"
     >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"/>
       <v-toolbar-title v-text="title"/>
       <v-spacer/>
       <v-toolbar-items>
@@ -17,26 +15,6 @@
     <v-content>
       <router-view/>
     </v-content>
-    <v-navigation-drawer
-      :clipped="true"
-      v-model="drawer"
-      disable-resize-watcher
-      app
-    >
-      <v-list 
-        class="pt-0" 
-        dense>
-        <v-divider/>
-        <v-list-tile :to="{ path: '/leagues' }">
-          <v-list-tile-action>
-            <v-icon>{{ menuItems[0].icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ menuItems[0].title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
   </v-app>
 </template>
 
